fbaab6eacfc89eb0f2484b294bdc6940f18ed084
HolySeraphim <bogdanov.pp@phystech.edu>
2024-01-07 20:58:10 +0300

    Clicked tiles
    It is clicked

--- desk/world.gd
+++ desk/world.gd
@@ -10,12 +10,15 @@ var pirate_scene: PackedScene = preload("res://tiles/pirate.tscn")
 func _ready():
 	websocket.execute_action.connect(_on_websocket_execute_action)
 	websocket.add_option.connect(_on_websocket_add_option)
+	var tilefield = Node2D.new()
+	tilefield.name = "TileField"
+	add_child(tilefield)
 	var field_size = [17, 17]
 	var water_range = [2, 2]
 	for x in field_size[0]:
 		for y in field_size[1]:
 			var tile = tile_scene.instantiate()
-			add_child(tile)
+			$TileField.add_child(tile)
 			tile.name = "tile_%s_%s" % [x - field_size[0] / 2, y - field_size[1] / 2]
 			tile.position = Vector2(x - int(field_size[0]) / 2, y - int(field_size[1]) / 2) * 200
 			if x < water_range[0] or x >= field_size[0] - water_range[0] or y < water_range[1] or y >= field_size[1] - water_range[1]:
@@ -33,7 +36,7 @@ func _on_websocket_execute_action(action: Dictionary):
 		ship.ship_generation(action["player_owner"])
 	elif action["type"] == "spawn_pirate" and action["pirate_type"] == "simple":
 		var pirate = pirate_scene.instantiate()
-		var tile = get_node("./tile_%s_%s" % [action["position"]["x"], action["position"]["y"]])
+		var tile = get_node("./TileField/tile_%s_%s" % [action["position"]["x"], action["position"]["y"]])
 		pirate.name = action["pirate_id"]
 		pirate.pirate_generation(action["player_owner"])
 		pirate.position = Vector2(action["position"]["x"], action["position"]["y"]) * 200
@@ -44,12 +47,12 @@ func _on_websocket_execute_action(action: Dictionary):
 	elif action["type"] == "move_pirate":
 		var pirate = find_child(action["pirate_id"], true, false)
 		var old_tile = pirate.get_parent()
-		var new_tile = get_node("./tile_%s_%s" % [action["position"]["x"], action["position"]["y"]])
+		var new_tile = get_node("./TileField/tile_%s_%s" % [action["position"]["x"], action["position"]["y"]])
 		pirate.position = Vector2(action["position"]["x"], action["position"]["y"]) * 200
 		old_tile.remove_child(pirate)
 		new_tile.add_child(pirate)
 	elif action["type"] == "open_card":
-		var tile = get_node("./tile_%s_%s" % [action["position"]["x"], action["position"]["y"]])
+		var tile = get_node("./TileField/tile_%s_%s" % [action["position"]["x"], action["position"]["y"]])
 		tile.open_frame(action["frame"], action["rotation"])
 	elif action["type"] == "ready_to_start":
 		$UI/WaitingFiller.visible = false
@@ -59,11 +62,11 @@ func _on_websocket_add_option(option: Dictionary):
 	if option["type"] == "move_pirate":
 		var pirate = find_child(option["pirate_id"], true, false)
 		var old_tile = pirate.get_parent()
-		var available_tile = get_node("./tile_%s_%s" % [option["position"]["x"], option["position"]["y"]])
+		var available_tile = get_node("./TileField/tile_%s_%s" % [option["position"]["x"], option["position"]["y"]])
 		var area = Area2D.new()
 		area.name = "area_%s_%s" % [option["position"]["x"], option["position"]["y"]]
 		available_tile.add_child(area)
-		area.input_event.connect(func(): _on_mouse_is_pressed(available_tile))
+		area.input_event.connect(func(node, event, shape_idx): _on_mouse_is_pressed(available_tile, event))
 		var shape = CollisionShape2D.new()
 		shape.name = "shape_%s_%s" % [option["position"]["x"], option["position"]["y"]]
 		area.add_child(shape)
@@ -71,5 +74,11 @@ func _on_websocket_add_option(option: Dictionary):
 		shape.shape.size = Vector2(200, 200)
 
 
+<<<<<<< Updated upstream
 func _on_mouse_is_pressed(tile):
 	print(1)
+=======
+func _on_mouse_is_pressed(tile, event):
+	if event is InputEventMouseButton and event.button_index == 1 and event.pressed:
+		pass
+>>>>>>> Stashed changes
